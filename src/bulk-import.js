9a630a0239a46e5d5e78219c8d3a953cd2984868
salus-sage <salus.sage@gmail.com>
2021-01-11 19:57:08 +0530

    connection var update

--- src/bulk-import.js
+++ src/bulk-import.js
@@ -1,11 +1,12 @@
 'use strict'
 
 require('array.prototype.flatmap').shim()
-const { Client } = require('@elastic/elasticsearch')
+/*const { Client } = require('@elastic/elasticsearch')
 const client = new Client({
   node: 'http://localhost:9200',
   log: "trace"
-})
+})*/
+var client= require ('./connection.js');
 
 async function run () {
   try {
